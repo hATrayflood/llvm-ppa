54fc00577a027b6f57d6c24752a6fe1e3a598718
hATrayflood <h.rayflood@gmail.com>
2014-05-23 00:25:48 +0900

    update llvm-toolchain 3.4.1-3 for precise.

--- llvm-toolchain-3.4/debian/orig-tar.sh
+++ llvm-toolchain-3.4/debian/orig-tar.sh
@@ -72,7 +72,7 @@ else
 fi
 
 if test -n "$RCRELEASE"; then
-    VERSION=$MAJOR_VERSION"~+"$TAG
+    VERSION=$MAJOR_VERSION"+"$REVISION # WAS TAG
     FULL_VERSION="llvm-toolchain-"$MAJOR_VERSION"_"$VERSION
 else
     VERSION=$MAJOR_VERSION"~svn"$REVISION
