47c4d087a77b6257d201ad4ad14abe8ffc1a0456
hATrayflood <h.rayflood@gmail.com>
2014-12-08 09:49:28 +0900

    add clang-3.0 for lucid.

---
+++ lucid/clang-3.0/clang-3.0/debian/rules.d/control.mk
@@ -0,0 +1,23 @@
+$(foreach var,BUILDDEPS BUILDCONFLICTS,$(eval \
+  $(if $(strip $($(var))),$(eval $(var) :=, $($(var))),$(eval $(var) :=))))
+
+extra_packages:=libclang1 libclang-dev libclang-common-dev python-clang
+packages := $(foreach build,$(builds),$($(build)_packages))
+packages += $(extra_packages) 
+
+debian/control:
+	@echo Regenerating control file...
+	sed -e "s/@PKGSOURCE@/$(PKGSOURCE)/g"			\
+	    -e "s/ *@BUILDDEPS@/$(BUILDDEPS)/g"			\
+	    -e "s/ *@BUILDCONFLICTS@/$(BUILDCONFLICTS)/g"	\
+	    -e "s/@UVERSION@/$(UVERSION)/g"			\
+	    -e "s/@GCC_VERSION@/$(GCC_VERSION)/g"		\
+	    -e "s/@PKG_VERSION@/$(pkg_version)/g"		\
+	    -e "s/ , /, /g"					\
+	    $@.in/source $(addprefix $@.in/,$(packages)) > $@
+
+control: debian/control
+
+clean: control
+
+.PHONY: clean control debian/control
